ace362ee021839c87d2aad005a3cbeccd89e2372
Joanna Preston <joannappreston@gmail.com>
2020-06-04 08:00:26 -0400

    initial commit - file structure

---
+++ models/scores.js
@@ -0,0 +1,16 @@
+module.exports = function (sequelize, DataTypes) {
+    var Score = sequelize.define("Score", {
+        score: DataTypes.INTEGER
+    });
+
+    Score.associate = function (models) {
+        Score.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+
+
+    return Score;
+};
