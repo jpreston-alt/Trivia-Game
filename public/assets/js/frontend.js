61577ad8197f8d68f3259a7c66364db31e58c516
Joanna Preston <joannappreston@gmail.com>
2020-06-09 23:33:38 -0400

    adds front end event handlers

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -1,6 +1,74 @@
 $(document).ready(function () {
 
+    // elements 
+    const $playBtn = $("#play-btn");
+    const $questionCont = $("#question-container");
+    const $chooseInput = $("#choose-input-group");
+    const $radioCont = $(".radio-container");
+    
+    let queryURL;
+
     init();
+
+    // event handlers
+    $playBtn.on("click", clickPlayBtn);
+
+
+    function clickPlayBtn(event) {
+        event.preventDefault();
+        
+        const $categoryID = $("#category-dropdown option:selected").data("id");
+        const $difficulty = $("#difficulty-dropdown option:selected").val().toLowerCase();
+
+        if ($categoryID != "default-drop" && $difficulty != "Select a Difficulty") {
+            queryURL = `https://opentdb.com/api.php?amount=10&category=${$categoryID}&difficulty=${$difficulty}&type=multiple`
+            // console.log(queryURL);
+
+            $chooseInput.addClass("display-none");
+            getQuestions();
+        };
+    };
+
+    function getQuestions() {
+        $.ajax({
+            method: "GET",
+            url: queryURL
+        }).then(data => {
+            console.log(data);
+            $questionCont.removeClass("display-none");
+            let question = data.results[0].question;
+            let corrAns = data.results[0].correct_answer;
+            let incAns = data.results[0].incorrect_answers;
+            let ansArr = [corrAns, incAns[0], incAns[1], incAns[2]];
+
+            $("#question").text(question);
+            renderRadios(ansArr);
+
+
+        })
+    };
+
+    // function renderQuestion(questions) {
+
+
+    // };
+
+    function renderRadios (arr) {
+
+        for (var i = 0; i < arr.length; i++) {
+            let radio =
+                `<div class="form-check">
+                    <input class="form-check-input" type="radio" value="${arr[i]}">
+                    <label class="form-check-label">
+                        ${arr[i]}
+                    </label>
+                </div>`
+
+            $radioCont.append(radio);
+        };
+
+
+    };
 
     function init() {
         $.ajax({
@@ -10,11 +78,13 @@ $(document).ready(function () {
             
             data.trivia_categories.forEach(el => {
                 let option = $("<option>");
+                option.attr("data-id", el.id);
                 option.text(`${el.name}`);
-                $("#choose-category-dropdown").append(option);
+                $("#category-dropdown").append(option);
             });
 
         });
     };
+
 
 });
